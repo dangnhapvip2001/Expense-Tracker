ffee9a945fdfa9016b40d478001c2be9b202f865
dangnhapvip2001 <thanh.lk195917@sis.hust.edu.vn>
2024-03-22 22:12:25 +0700

    first commit

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // Các thẻ expense
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              expense.title,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge, // chỉnh giống theme titleLarge ở main
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense
+                        .category]), // categoryIcons[expense.category] = categoryIcons[Category.food] = Icons.lunch_dining()
+                    const SizedBox(width: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
